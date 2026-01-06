ce9ce7beb2421c7a57b55b5b65389d0750fc0eb3
Rahul Ukey <141477302+rahuluke@users.noreply.github.com>
2026-01-07 01:17:17 +0530

    Refactor middleware token verification logic

--- middleware.js
+++ middleware.js
@@ -2,11 +2,13 @@ import { NextResponse } from "next/server";
 
 export async function middleware(request) {
   const verifyToken = async (token) => {
-    let req = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/verify`, {
-      body: JSON.stringify({ token }),
+    return await fetch(`${request.nextUrl.origin}/api/verify`, {
       method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ token }),
     });
-    return req;
   };
 
   if (request.nextUrl.pathname.startsWith("/login")) {
@@ -14,25 +16,26 @@ export async function middleware(request) {
 
     if (token) {
       const response = await verifyToken(token.value);
-      if (response.status == 200) {
-        return NextResponse.redirect(new URL("/admin/bloods", request.url)); // if token is vailed redirect the user
+      if (response.status === 200) {
+        return NextResponse.redirect(new URL("/admin/bloods", request.url));
       }
     }
   }
+
   if (request.nextUrl.pathname.startsWith("/admin")) {
     const token = request.cookies.get("token");
 
-    if (token) {
-      const response = await verifyToken(token.value);
-      if (response.status != 200) {
-        return NextResponse.redirect(new URL("/login", request.url)); // if token is vailed redirect the user
-      }
-    } else {
+    if (!token) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    const response = await verifyToken(token.value);
+    if (response.status !== 200) {
       return NextResponse.redirect(new URL("/login", request.url));
     }
   }
 
-  if (request.nextUrl.pathname == "/admin") {
-    return NextResponse.redirect(new URL("/admin/bloods", request.url)); // if token is vailed redirect the user
+  if (request.nextUrl.pathname === "/admin") {
+    return NextResponse.redirect(new URL("/admin/bloods", request.url));
   }
 }
