2242e6825e2787dd55a518eb9daabfafd27446c9
Rahul Ukey <141477302+rahuluke@users.noreply.github.com>
2026-01-10 15:44:11 +0530

    Await database connection in GET function

--- app/api/blood_db/route.js
+++ app/api/blood_db/route.js
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
 
     try {
-        ConnectToDB()
+       await ConnectToDB()
 
         let allEntries = await Doners.find({})
         return NextResponse.json({ data: allEntries })
